87189bd7e705609e677fe2161bd37429f2485e61
IlkinAkhmed <tu7mp4w99@code.edu.az>
2023-12-20 01:12:28 +0400

    login

--- server/index.js
+++ server/index.js
@@ -1,27 +1,43 @@
 const express = require("express");
 const { checkUser } = require("./src/middleware/checkUser");
+const jwt = require("jsonwebtoken")
 const app = express();
 const PORT = 5000;
 app.use(express.json());
 app.use(express.static("public"));
+const PrivateKey = 'sdf552344@!#w__'
 
-// const MyLogger = (req, res, next) => {
-//   console.log("LOGGED");
-//   next();
-// };
+const users = [
+  {
+    username: "IlkinAkhmed",
+    password: "ilkin123"
+  },
+  {
+    username: "Samir",
+    password: "samir123"
+  },
+  {
+    username: "Tural",
+    password: "tural23"
+  },
+]
 
-// const MyDate = (req, res, next) => {
-//   console.log(new Date(Date.now()));
-//   next();
-// };
 
-// app.get("/products", MyDate, (req, res) => {
-//   res.send("Prdoucts");
-// });
+app.post("/login", (req, res) => {
+  const { username, password } = req.body
+  const user = users.find(x => x.username === username)
+  if (!user) {
+    res.status(404).send('User not found!!')
+  }
+  if (user.password !== password) {
+    res.status(403).send("wrong details!!!");
+  }
+  const token = jwt.sign({ username: username }, PrivateKey)
+  res.send(token)
+});
 
-app.post("/login", checkUser, (req, res) => {
-  console.log("user logined");
-  res.send("welcome to your account");
+app.get("/login", (req, res) => {
+  res.send(users);
 });
 
 app.listen(PORT, () => {
